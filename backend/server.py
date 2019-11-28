d2b49e3cce1f7858de91cc98a82f3930b321f9ba
Andrey Gordeev <prolodgy1997@gmail.com>
2019-11-28 12:49:23 +0300

    React and redux ready to work, postgres is also looking pretty good

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,14 @@
+
 import os.path
 
 import flask
+from flask import request, jsonify
 import flask_cors
+from flask_sqlalchemy import SQLAlchemy
 
+POSTGRES_USER="vkr"
+POSTGRES_PW="vkr"
+POSTGRES_DB="eapml"
 
 class VKR(flask.Flask):
 
@@ -14,8 +20,40 @@ class VKR(flask.Flask):
         # Подтюним его когда-нибудь потом.
         flask_cors.CORS(self)
 
-app = VKR('vkr')
-
 env = os.environ.get('APP_ENV', 'dev')
 print(f'Starting application in {env} mode')
-app.config.from_object(f'backend.{env}_settings')
+
+app = VKR('vkr')
+DB_URL = 'postgresql://{user}:{pw}@postgres/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,db=POSTGRES_DB)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from backend.models import User
+
+@app.route("/")
+def hello():
+    return "Hello, world!"
+
+@app.route("/add")
+def add_user():
+    email = request.args.get('email')
+    password = request.args.get('password')
+    try:
+        user = User(
+            email = email,
+            password = password
+        )
+        db.session.add(user)
+        db.session.commit()
+        return "User added. User id = {}, email = {}, password = {}".format(user.id, user.email, user.password)
+    except Exception as e:
+        return (str(e))
+
+@app.route("/api/v1/users/<int:id_>", methods=["GET"])
+def get_by_id(id_):
+    try:
+        user = User.query.filter_by(id=id_).first()
+        return jsonify(user.serialize())
+    except Exception as e:
+        return (str(e))
--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {Provider} from 'react-redux'
 import App from './components/app'
+import configureStore from './redux/configureStore'
+
+// Нужно создать то самое централизованное хранилище...
+const store = configureStore();
 
 render(
-    <App/>,
+    // ... и завернуть приложение в специальный компонент,
+    // умеющий с ним работать
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.querySelector('#app')
 );
